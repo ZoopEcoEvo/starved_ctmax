c51f1d3fc92542f7cf127465385be38c83231b8c
Matthew Sasaki <matthewsasaki@Matthews-MacBook-Pro.local>
2022-11-05 01:45:33 -0400

    Removed the github_document output of the render since the PDF draft looks fine.

--- Scripts/02_make_report.R
+++ Scripts/02_make_report.R
@@ -1,7 +1,7 @@
 library(rmarkdown)
 
 process_all_data = F
-make_figures = F
+make_figures = T
 
 #Process the temperature and time data to estimate CTmax values
 source(file = "Scripts/01_data_processing.R")
@@ -32,5 +32,5 @@ starv_data = full_data %>%
 #Render the manuscript draft
 render(input = "Manuscript/Sasaki_and_Moreno_2023.Rmd", #Input the path to your .Rmd file here
        output_file = paste("draft_", Sys.Date(), ".pdf", sep = ""), #Name your file here; as it is, this line will create reports named with the date
-       output_dir = "Output/Drafts/", #Set the path to the desired output directory here
-       output_format = c("pdf_document","github_document"))
+       output_dir = "Output/Drafts/") #Set the path to the desired output directory here
+       #output_format = c("pdf_document","github_document"))
