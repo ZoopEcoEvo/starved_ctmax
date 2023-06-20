31b1201672c03a39608f3b11069a4f42a95d45eb
matt-sasaki <56654872+matt-sasaki@users.noreply.github.com>
2023-06-19 22:25:25 -0400

    Fixing version

--- Scripts/02_make_report.R
+++ Scripts/02_make_report.R
@@ -1,7 +1,9 @@
 library(rmarkdown)
+library(tidyverse)
 
 process_all_data = F
 make_figures = F
+knit_manuscript = T
 
 #Process the temperature and time data to estimate CTmax values
 source(file = "Scripts/01_data_processing.R")
@@ -13,6 +15,11 @@ ramp_record = read.csv(file = "Output/Data/ramp_record.csv")
 
 #Make Figures
 if(make_figures == T){
+  
+  # Cell concentration data to calculate grazing rates
+  cell_data = readxl::read_excel(path = "Data/grazing_test.xlsx") %>% 
+    drop_na(cells)
+  
   render(input = "Output/Reports/project_figures.Rmd", #Input the path to your .Rmd file here
          output_file = "project_figures.html") #Name your file here; as it is, this line will create reports named with the date
 }
@@ -30,12 +37,11 @@ starv_data = full_data %>%
   group_by(rep) %>% 
   mutate("rep_day" = cumul_day - first(cumul_day))
 
-# Cell concentration data to calculate grazing rates
-cell_data = readxl::read_excel(path = "Data/grazing_test.xlsx") %>% 
-  drop_na(cells)
-
-#Render the manuscript draft
-render(input = "Manuscript/Sasaki_and_Moreno_2023.Rmd", #Input the path to your .Rmd file here
-       output_file = paste("draft_", Sys.Date(), ".pdf", sep = ""), #Name your file here; as it is, this line will create reports named with the date
-       output_dir = "Output/Drafts/") #Set the path to the desired output directory here
-       #output_format = c("pdf_document","github_document"))
+if(knit_manuscript == T){
+  #Render the manuscript draft
+  render(input = "Manuscript/Moreno_and_Sasaki_2023.Rmd", #Input the path to your .Rmd file here
+         output_file = "Moreno_and_Sasaki_2023", #Name your file here; as it is, this line will create reports named with the date
+         output_dir = "Output/Drafts/",
+         output_format = "all",
+         clean = T)
+}
